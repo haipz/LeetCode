d2544f7a2546f04aa90f9ecf213b43b9f3bea645
Haipz <haipzm@gmail.com>
2016-03-16 16:47:18 +0800

    Create 13. Roman to Integer | Easy | Math, String.cpp

---
+++ 13. Roman to Integer | Easy | Math, String.cpp
@@ -0,0 +1,38 @@
+/*
+Given a roman numeral, convert it to an integer.
+
+Input is guaranteed to be within the range from 1 to 3999.
+*/
+// I（1）、V（5）、X（10）、L（50）、C（100）、D（500）、 M（1000）
+class Solution {
+public:
+    int romanToInt(string s) {
+        int nums[] = {1, 5, 10, 50, 100, 500, 1000}, res = 0;
+        string chs("IVXLCDM??");
+        for (int i = 0; i < s.size(); ++i) {
+            int curi = chs.find(s[i]);
+            if (curi & 1) {
+                res += nums[curi];
+                while (i + 1 < s.size() && curi > 0 && chs[curi - 1] == s[i + 1]) {
+                    res += nums[curi - 1];
+                    ++i;
+                }
+            } else {
+                if (i + 1 < s.size() && chs[curi + 2] == s[i + 1]) {
+                    res += nums[curi + 2] - nums[curi];
+                    ++i;
+                } else if (i + 1 < s.size() && chs[curi + 1] == s[i + 1]) {
+                    res += nums[curi + 1] - nums[curi];
+                    ++i;
+                } else {
+                    res += nums[curi];
+                    while (i + 1 < s.size() && chs[curi] == s[i + 1]) {
+                        res += nums[curi];
+                        ++i;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+};
